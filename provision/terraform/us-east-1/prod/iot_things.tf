e3f4837ba8d33c526d84432a43ecdc23f0df76cb
@slovacus <slovacus@gmail.com>
2019-09-28 17:32:25 -0500

    feat: validate csr unnecessary (#3)

--- provision/terraform/us-east-1/prod/iot_things.tf
+++ provision/terraform/us-east-1/prod/iot_things.tf
@@ -1,14 +1,10 @@
-/*
- * IOT things configuration
- */
-
 resource "aws_iot_thing" "air-pollution" {
   name            = "air-pollution"
   thing_type_name = "air-pollution"
+  # MQ2 (Humo), MQ5 (GLP), MQ7 (CO), MQ135 (CO2) y DHT22
 }
 
 resource "aws_iot_certificate" "cert_air-pollution" {
-  # csr    = file("/keybase/private/luismayta/pem/terraform-iot-air-pollution-prod.pem")
   active = true
 }
 
