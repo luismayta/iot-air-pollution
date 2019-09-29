43bcc2d6a0f9b3923f0fd77d8f3e64638a161b7e
@slovacus <slovacus@gmail.com>
2019-09-29 01:10:36 -0500

    (#5) chore: output for certificate iot

--- provision/terraform/us-east-1/prod/output.tf
+++ provision/terraform/us-east-1/prod/output.tf
@@ -27,3 +27,28 @@ output "aws_sqs_queue_air_pollution_arn" {
   description = "arn aws_sqs_queue_air_pollution"
   value       = aws_sqs_queue.air-pollution_queue.arn
 }
+
+output "aws_iot_certificate_air-pollution_idn" {
+  description = "id air pollution"
+  value       = aws_iot_certificate.air-pollution.id
+}
+
+output "aws_iot_certificate_air-pollution_arn" {
+  description = "arn air pollution"
+  value       = aws_iot_certificate.air-pollution.arn
+}
+
+output "aws_iot_certificate_air-pollution_certificate_pem" {
+  description = "certificate pem air pollution"
+  value       = aws_iot_certificate.air-pollution.certificate_pem
+}
+
+output "aws_iot_certificate_air-pollution_public_key" {
+  description = "public key air pollution"
+  value       = aws_iot_certificate.air-pollution.public_key
+}
+
+output "aws_iot_certificate_air-pollution_private_key" {
+  description = "private key air pollution"
+  value       = aws_iot_certificate.air-pollution.private_key
+}
