511a87e81589e9de3c52c9a98f4b0b2835efcd62
@slovacus <slovacus@gmail.com>
2019-09-29 01:08:34 -0500

    (#5) chore: generated file certificate keys

--- provision/terraform/us-east-1/prod/iot_things.tf
+++ provision/terraform/us-east-1/prod/iot_things.tf
@@ -1,34 +1,33 @@
 resource "aws_iot_thing" "air-pollution" {
   name            = "air-pollution"
-  thing_type_name = "air-pollution"
+  thing_type_name = "pollution"
 }
 
-resource "tls_cert_request" "air-pollution" {
-  key_algorithm   = "RSA"
-  private_key_pem = file(var.private_key)
-
-  subject {
-    common_name    = "Air Pollution"
-    organization   = "Air Pollution, Inc"
-    street_address = ["Kettwiger Str. 20"]
-    locality       = "Lima"
-    postal_code    = "380000"
-    province       = "Lima"
-    country        = "Peru"
-  }
-}
-
-resource "aws_iot_certificate" "cert_air-pollution" {
-  csr    = tls_cert_request.air-pollution.cert_request_pem
+resource "aws_iot_certificate" "air-pollution" {
   active = true
 }
 
-resource "aws_iot_thing_principal_attachment" "att_air-pollution" {
-  principal = aws_iot_certificate.cert_air-pollution.arn
+resource "local_file" "certificate_pem" {
+  content  = aws_iot_certificate.air-pollution.certificate_pem
+  filename = var.file_crt
+}
+
+resource "local_file" "public_key" {
+  content  = aws_iot_certificate.air-pollution.public_key
+  filename = var.public_key
+}
+
+resource "local_file" "private_key" {
+  content  = aws_iot_certificate.air-pollution.private_key
+  filename = var.private_key
+}
+
+resource "aws_iot_thing_principal_attachment" "air-pollution" {
+  principal = aws_iot_certificate.air-pollution.arn
   thing     = aws_iot_thing.air-pollution.name
 }
 
 resource "aws_iot_policy_attachment" "patt_air-pollution" {
   policy = aws_iot_policy.air-pollution_pubsub.name
-  target = aws_iot_certificate.cert_air-pollution.arn
+  target = aws_iot_certificate.air-pollution.arn
 }
