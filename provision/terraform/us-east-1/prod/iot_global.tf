b4140c3a7e15a757c20224f2d5c2f3cf1ef977ac
@slovacus <slovacus@gmail.com>
2019-10-05 18:03:43 -0500

    (#9) chore: global iot global

--- provision/terraform/us-east-1/prod/iot_global.tf
+++ provision/terraform/us-east-1/prod/iot_global.tf
@@ -6,68 +6,21 @@ resource "aws_iot_thing_type" "air-pollution" {
   name = "pollution"
 }
 
-resource "aws_iot_policy" "air-pollution_pubsub" {
-  name = "Air-PollutionPubSub"
-
-  policy = <<EOF
-{
-        "Version": "2012-10-17",
-        "Statement": [
-        {
-            "Action": ["iot:*"],
-            "Effect": "Allow",
-            "Resource": "*"
-            }
-        ]
-}
-EOF
-}
-
-resource "aws_iam_role" "air-pollution_iot_iam_role" {
-  name = "air-pollution_iot_iam_role"
-
-  assume_role_policy = <<EOF
-{
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "iot.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "air-pollution_iot_iam_role_policy" {
-  name = "air-pollution_iot_iam_role_policy"
-  role = aws_iam_role.air-pollution_iot_iam_role.id
-
-  policy = <<EOF
-{
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Effect": "Allow",
-            "Action": [
-                "sqs:SendMessage"
-            ],
-            "Resource": "*"
-        }]
-}
-EOF
-}
-
 resource "aws_iot_topic_rule" "air_pollution_sensor_rule" {
   name        = "air_pollution_sensor_rule"
-  description = "MQTT topic for air pollution sensor messages"
+  description = "Kinesis Rule"
   enabled     = true
   sql         = "SELECT * FROM 'air-pollution/events'"
   sql_version = "2016-03-23"
 
-  sqs {
-    queue_url  = aws_sqs_queue.air-pollution_queue.id
-    role_arn   = aws_iam_role.air-pollution_iot_iam_role.arn
-    use_base64 = false
+  kinesis {
+    role_arn      = aws_iam_role.air-pollution.arn
+    stream_name   = aws_kinesis_stream.air-pollution.name
+    partition_key = "$${newuuid()}"
+  }
+
+  firehose {
+    delivery_stream_name = aws_kinesis_firehose_delivery_stream.air-pollution.name
+    role_arn             = aws_iam_role.air-pollution.arn
   }
 }
