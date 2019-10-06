9140e4a9b6ff92aa41e491de6c1c787e7c452ed0
@slovacus <slovacus@gmail.com>
2019-10-05 21:19:26 -0500

    chore: key aws (#11)

---
+++ provision/terraform/us-east-1/prod/creds.tf
@@ -0,0 +1,11 @@
+#
+# AWS Key pair
+#
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(lookup(var.aws_keys, "pub"))
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
