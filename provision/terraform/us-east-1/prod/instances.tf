ef5838e857d59cd09efcd63617d1d8b708599cf1
@slovacus <slovacus@gmail.com>
2019-10-05 21:18:28 -0500

    (#11) chore: instance ec2 server database

---
+++ provision/terraform/us-east-1/prod/instances.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "db" {
+  ami = data.aws_ami.ubuntu.id
+  lifecycle {
+    ignore_changes = ["ami"]
+  }
+  instance_type        = var.aws_instance_type
+  iam_instance_profile = aws_iam_instance_profile.air-pollution.name
+  key_name             = aws_key_pair.auth.key_name
+  vpc_security_group_ids = [
+    aws_security_group.main.id,
+    aws_security_group.postgresql.id,
+  ]
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} ${self.private_ip} > file.log"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = lookup(var.user, "ssh_user")
+      private_key = file(lookup(var.aws_keys, "pem"))
+      host        = aws_instance.db.public_ip
+    }
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get install -y python vim git-core python-pip",
+      "touch ~/provisioned", # Troll
+    ]
+  }
+
+  tags = {
+    terraform_id = "${var.stage}-terraform"
+    Name         = "${var.namespace}-${var.stage}"
+    Stage        = var.stage
+    Namespace    = var.namespace
+  }
+
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.db.id
+  vpc      = true
+}
